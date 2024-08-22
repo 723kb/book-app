8ec0f3489c127d2616660fd46815816701d82105
723kb <pp5_vb.swd723@icloud.com>
2024-08-22 23:45:20 +0900

    build Done

---
+++ public/build/assets/InputLabel-BZrFtUG9.js
@@ -0,0 +1,1 @@
+import{j as m}from"./app-to61IkPx.js";function o({value:t,className:e="",children:r,...s}){return m.jsx("label",{...s,className:"block font-medium text-sm text-gray-700 "+e,children:t||r})}export{o as I};
